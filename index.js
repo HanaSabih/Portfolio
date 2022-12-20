89e8796c7373f0b27cf0ab3cb5204760ba2308a8
Alwyn A. S <114167157+Thoth1111@users.noreply.github.com>
2022-12-20 18:30:11 +0300

    Modify index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const icon = document.querySelector(".icon");
 const x = document.getElementById("myTopnav");
 
+icon.addEventListener("click", () => {
+  x.classList.toggle("responsive");
+});
+
 document.querySelectorAll(".rightNav").forEach((n) =>
   n.addEventListener("click", () => {
     x.classList.remove("responsive");
