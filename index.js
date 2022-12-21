709ed8d1ff669311c244d69a6459deaf78b30add
Hana Sabih <hanasabih@gmail.com>
2022-12-21 06:37:32 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const body = document.body;
 const icon = document.querySelector(".icon");
 const x = document.getElementById("myTopnav");
 
 icon.addEventListener("click", () => {
   x.classList.toggle("responsive");
+  body.classList.toggle("responsive");
 });
 
 document.querySelectorAll(".rightNav").forEach((n) =>
