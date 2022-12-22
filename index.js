0f9e0e098e3222d5581c35e8145fe6c87cf9964a
hana <hanasabih@gmail.com>
2022-12-22 10:52:58 +0200

    fix problems

--- index.js
+++ index.js
@@ -1,102 +1,96 @@
-const body = document.body;
-const icon = document.querySelector(".icon");
-const workCards = document.querySelector(".workCards");
-const x = document.getElementById("myTopnav");
+const { body } = document;
+const icon = document.querySelector('.icon');
+const workCards = document.querySelector('.workCards');
+const x = document.getElementById('myTopnav');
 
 // navbar
-icon.addEventListener("click", () => {
-  x.classList.toggle("responsive");
-  body.classList.toggle("responsive");
+icon.addEventListener('click', () => {
+  x.classList.toggle('responsive');
+  body.classList.toggle('responsive');
   // document.body.style.overflow = "hidden";
 });
-document.querySelectorAll(".rightNav").forEach((n) =>
-  n.addEventListener("click", () => {
-    x.classList.remove("responsive");
-  })
-);
+document.querySelectorAll('.rightNav').forEach((n) => n.addEventListener('click', () => {
+  x.classList.remove('responsive');
+}));
 // data
 const projects = [
   {
-    id: "0",
-    imgSouce: "./images/work/work1.png",
-    title: "Tonic",
-    tags: ["html", "Ruby on rails", "css", "js"],
+    id: '0',
+    imgSouce: './images/work/work1.png',
+    title: 'Tonic',
+    tags: ['html', 'Ruby on rails', 'css', 'js'],
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
   },
   {
-    id: "1",
-    imgSouce: "./images/work/work2.png",
-    title: "Multi-Post Stories",
-    tags: ["html", "Ruby on rails", "css", "js"],
+    id: '1',
+    imgSouce: './images/work/work2.png',
+    title: 'Multi-Post Stories',
+    tags: ['html', 'Ruby on rails', 'css', 'js'],
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
   },
   {
-    id: "2",
-    imgSouce: "./images/work/work3.png",
-    title: "Facebook 360",
-    tags: ["html", "Ruby on rails", "css", "js"],
+    id: '2',
+    imgSouce: './images/work/work3.png',
+    title: 'Facebook 360',
+    tags: ['html', 'Ruby on rails', 'css', 'js'],
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
   },
   {
-    id: "3",
-    imgSouce: "./images/work/work4.png",
-    title: "Uber Navigation",
-    tags: ["html", "Ruby on rails", "css", "js"],
+    id: '3',
+    imgSouce: './images/work/work4.png',
+    title: 'Uber Navigation',
+    tags: ['html', 'Ruby on rails', 'css', 'js'],
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
   },
   {
-    id: "4",
-    imgSouce: "./images/work/work5.png",
-    title: "Profesional Art Printing Data",
-    tags: ["html", "Ruby on rails", "css", "js"],
+    id: '4',
+    imgSouce: './images/work/work5.png',
+    title: 'Profesional Art Printing Data',
+    tags: ['html', 'Ruby on rails', 'css', 'js'],
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
   },
   {
-    id: "5",
-    imgSouce: "./images/work/work6.png",
-    title: "Multi-Post Stories Gain+Glory",
-    tags: ["html", "Ruby on rails", "css", "js"],
+    id: '5',
+    imgSouce: './images/work/work6.png',
+    title: 'Multi-Post Stories Gain+Glory',
+    tags: ['html', 'Ruby on rails', 'css', 'js'],
     text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
   },
 ];
-
-for (let i in projects) {
-  let workListCard = document.createElement("div");
-  let workListag = document.createElement("li");
-  workListCard.classList.add("card");
+projects.forEach((project) => {
+  // for (const i in projects) {
+  const workListCard = document.createElement('div');
+  workListCard.classList.add('card');
   workListCard.innerHTML = `
 <div class="card_container">
-            <h4>${projects[i].title}</h4>
+            <h4>${project.title}</h4>
             <div class="tags">
               <ul class="listContainerModal">
-                <li><span>${projects[i].tags[0]}</span></li>
-                <li><span>${projects[i].tags[1]}</span></li>
-                <li><span>${projects[i].tags[2]}</span></li>
-                <li><span>${projects[i].tags[3]}</span></li>
+                <li><span>${project.tags[0]}</span></li>
+                <li><span>${project.tags[1]}</span></li>
+                <li><span>${project.tags[2]}</span></li>
+                <li><span>${project.tags[3]}</span></li>
               </ul>
             </div>
-            <button id=${projects[i].id} class="btnModal">See Project</button>
+            <button id=${project.id} class="btnModal">See Project</button>
           </div>
 
         `;
 
   workCards.appendChild(workListCard);
-}
+});
 
 // Get the modal
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-const btnModal = document.querySelector(".btnModal");
+const modal = document.getElementById('myModal');
 
-document.querySelectorAll(".btnModal").forEach((n) =>
-  n.addEventListener("click", (e) => {
-    let idname = e.target.id;
+document.querySelectorAll('.btnModal').forEach((n) => n.addEventListener('click', (e) => {
+  const idname = e.target.id;
 
-    modal.style.display = "block";
+  modal.style.display = 'block';
 
-    let modalDiv = document.createElement("div");
-    modalDiv.classList.add("modal-content");
-    modalDiv.innerHTML = `
+  const modalDiv = document.createElement('div');
+  modalDiv.classList.add('modal-content');
+  modalDiv.innerHTML = `
   <span class="closeModal">&times;</span>
   <img src=${projects[idname].imgSouce} alt=${projects[idname].title}/>
   <div class="hdrModal ">
@@ -122,16 +116,18 @@ document.querySelectorAll(".btnModal").forEach((n) =>
   
 
         `;
-    modal.appendChild(modalDiv);
-    document.querySelector(".closeModal").onclick = function () {
-      modal.style.display = "none";
-      modal.innerHTML = "";
-    };
-    window.onclick = function (event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-        modal.innerHTML = "";
-      }
-    };
-  })
-);
+  modal.appendChild(modalDiv);
+  document.querySelector('.closeModal').onclick = function () {
+    modal.style.display = 'none';
+    modal.innerHTML = '';
+  };
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+      modal.innerHTML = '';
+    }
+  };
+  body.classList.toggle('responsive');
+}));
+
+// ==========Form Validation==========
